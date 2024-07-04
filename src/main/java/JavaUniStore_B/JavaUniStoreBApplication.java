9dc43e60ca5474ea1bb3c760b91ee7d6fe0d2958
kkm4512 <nayoun440@naver.com>
2024-07-04 11:31:51 +0900

    first

---
+++ src/main/java/JavaUniStore_B/JavaUniStoreBApplication.java
@@ -0,0 +1,13 @@
+package JavaUniStore_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUniStoreBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUniStoreBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaUniStore_B/JavaUniStoreBApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaUniStore_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUniStoreBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
